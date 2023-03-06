ea900dcd9e9fbf46c3f5c12be3ecf758a324f1a4
Jartius <JStavey13@users.noreply.github.com>
2023-03-05 22:21:47 -0500

    changed homepage

--- my-app/src/Components/nav.js
+++ my-app/src/Components/nav.js
@@ -3,17 +3,7 @@ import React from 'react';
 function Nav() {
     return (
     <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-    <a class="navbar-item" href="/">
-      <img src="" width="112" height="28"/>
-    </a>
-  
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true">About</span>
-        <span aria-hidden="true">Projects</span>
-        <span aria-hidden="true">Contact</span>
-      </a>
-    </div>
+
   
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
