83d3a1f2aab954f2221717c9bc7cea2b7b6c62ef
Jartius <JStavey13@users.noreply.github.com>
2023-05-07 22:00:43 -0400

    gravado image displays

--- my-app/src/Components/portfolio.js
+++ my-app/src/Components/portfolio.js
@@ -6,21 +6,25 @@ function Portfolio() {
 
   return (
 
+
+  <section id='entries'>
     <Row xs={1} md={2} className="g-4">
         {data.map((project, idx) => (
           <Col key={idx}>
-    <Card className="bg-dark text-white">
-      <Card.Img src={`../assets/image/${project.image}`} alt="Card image" />
+    <Card className="bg-dark text-white" id='card'>
+      <Card.Img src={`public/assets/image/${project.image}`} alt="Card image" id="item-img" />
       <Card.ImgOverlay>
         <Card.Title>{project.title}</Card.Title>
         <Card.Text>
+          <p>
           {project.description}
+          </p>
         </Card.Text>
-        <Card.Text>     </Card.Text>
       </Card.ImgOverlay>
     </Card>
     </Col>))}
     </Row>
+    </section>
     );
         }
 
