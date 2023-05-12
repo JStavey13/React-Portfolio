de34bbd00df10ded6da043f0249249ea6c70d42e
Jartius <JStavey13@users.noreply.github.com>
2023-05-12 13:57:27 -0400

    download button for resume

---
+++ my-app/src/Components/resume.js
@@ -0,0 +1,20 @@
+
+
+
+export default function Resume() {
+
+    return (
+        <div class="display">
+            <h1>Resume</h1>
+            <img src="https://docs.google.com/document/d/1oC9DMIq3bcfpZJN4aoXy9Qr36JbH9_ClVjCgumoYBO4/edit?usp=sharing" 
+            alt="resume"
+            id="resume"
+            ></img>
+            <div className="header_buttons">
+                <a href="././public/assets/image/Resume.pdf" className="btn btn-outline" download>Download Resume</a>
+            </div>
+
+
+        </div>
+    )
+}
