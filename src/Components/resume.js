9ceafaa1afcc50a539d989ddfc0159a1bd8ca216
Jartius <JStavey13@users.noreply.github.com>
2023-05-16 13:55:49 -0400

    refactored resume part of resume page

--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,27 +1,58 @@
-
+import resume from "../image/resume.jpg"
+import resumeDownload from "../image/Resume.pdf"
 
 
 export default function Resume() {
 
     return (
-        <div class="display">
-            <h1 class="qual-header">Resume
-            <p>
-            <a href="././public/assets/image/Resume.pdf" className="download-btn" download>Download Resume</a></p></h1>
-            <img src="././public/assets/image/resume.jpg" 
-            alt="resume"
-            id="resume"
+        <div className="display">
+{/* resume header */}
+            <h1 style={{
+                marginTop: "40px",
+                outlineStyle: "solid",
+                backgroundColor: "white",
+                borderRadius: "10px",
+                padding: "10px",
+                textAlign: "center",
+            }}>
+                Resume
+{/* resume download button */}
+                <p>
+                    <a href={resumeDownload} className="download-btn" download>
+                        Download Resume
+                    </a>
+                </p>
+            </h1>
+{/* resume picture */}
+            <img src={resume}
+                alt="resume"
+                style={{
+                    display: "block",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    outlineStyle: "solid",
+                    borderRadius: "40px",
+                    width: "55%",
+                }}
             ></img>
 
-
-            <h1 class="qual-header">Bootcamp Certificate
-            <p>
-            <a href="././public/assets/image/bootcampCert.pdf" className="download-btn" download>Download</a>
-            </p>
+{/* bootcamp cert header */}
+            <h1 style={{
+                marginTop: "40px",
+                outlineStyle: "solid",
+                backgroundColor: "white",
+                borderRadius: "10px",
+                padding: "10px",
+                textAlign: "center",
+            }}>
+                Bootcamp Certificate
+                <p>
+                    <a href="././public/assets/image/bootcampCert.pdf" className="download-btn" download>Download</a>
+                </p>
             </h1>
-            <img src="././public/assets/image/bootcampCert.jpg" 
-            alt="resume"
-            id="resume"
+            <img src="././public/assets/image/bootcampCert.jpg"
+                alt="resume"
+                id="resume"
             ></img>
 
         </div>
