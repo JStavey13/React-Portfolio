cce5aedda3e204709dbeba76fb1e6a3890ccbbfa
Jartius <JStavey13@users.noreply.github.com>
2023-06-01 12:39:28 -0400

    changed footer color

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -9,15 +9,15 @@ function Footer() {
           bottom: "0",
           width: "100%",
           
-          backgroundColor: 'black'
+          backgroundColor: '#a6aeba'
 
 
         }}
           class="footer">
-  <div class="content has-text-centered" style={{ color: 'green', fontSize: '15px' }}>
+  <div class="content has-text-centered" style={{ color: 'black', fontSize: '15px' }}>
     <p class="footer-text">
       <strong>Portfolio</strong> by <a href="https://github.com/jstavey13">Jarrett Stavey</a>. The source code is licensed as:
-      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. 
+      <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. 
       designed using <a href='https://getbootstrap.com/'>Bootstrap</a> and sourced on <a href='https://github.com/jstavey13/react-portfolio'>Github</a>.
     </p>
   </div>
